fe91286a13d4243c3097393e96cc669e510830d4
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-03 22:46:34 +0530

    Time: 0 ms (100%), Space: 44.2 MB (48.61%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -7,10 +7,6 @@ class Solution {
         for(int i=0;i<mat.length;i++){
             sum+=mat[(mat.length-1)-i][i];
         }
-        if(mat.length==1){
-            sum-=mat[0][0];
-            return sum;
-        }
         if(mat.length%2!=0){
             sum-=mat[(mat.length)/2][(mat.length/2)];
         }
