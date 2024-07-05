20ddf13055be307c7aec1f2106f001499d7d627d
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-05 18:24:12 +0530

    Time: 1 ms (71.71%), Space: 45.4 MB (27.73%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int m=mat.length;
-        int n=mat[0].length;
-        if(r*c!=m*n){
+        int[][] arr= new int[r][c];
+        int col=0;
+        int row=0;
+        if(r*c!=mat.length*mat[0].length){
             return mat;
         }
-        List<Integer> temp = new ArrayList<Integer>();
-        int[][] ans = new int[r][c];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                temp.add(mat[i][j]);
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[i].length; j++){
+                arr[row][col]=mat[i][j];
+                if(col<c-1){
+                    col++;
+                }
+                else if(row<r-1){
+                    row++;
+                    col=0;
+                }
             }
-            
         }
-        int pt=0;
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                ans[i][j]=temp.get(pt);
-                pt++;
-            }
-            
-        }
-        return ans;
+        return arr;
     }
 }
