c6cbb5945aa510640e2a85b544b8fb1c6cc8c20c
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-10 19:20:34 +0530

    Time: 1 ms (86.7%), Space: 40.5 MB (85.17%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int s=1;
+        int e=x;
+        while(s<=e){
+            long mid = s + (e-s) / 2;
+            if(mid*mid==x){
+                return (int)mid;
+            }
+            else if(mid*mid>x){
+                e=(int)mid-1;
+            }
+            else{
+                s=(int)mid+1;
+            }
+        }
+        return e;
+    }
+}
