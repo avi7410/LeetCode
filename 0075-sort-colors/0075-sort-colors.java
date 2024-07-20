19bcaa887c8b7fd2d6ecb5f5f57d32c1a088c893
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-21 00:32:32 +0530

    Time: 1 ms (21.06%), Space: 41.5 MB (74.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,36 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int red=0;
-        int white=0;
-        int blue=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==0){
-                red++;
-            }
-            else if(nums[i]==1){
-                white++;
-            }
-            else{
-                blue++;
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j>0; j--){
+                if(nums[j]<nums[j-1]){
+                    swap(nums, j, j-1);
+                }
+                else{
+                    break;
+                }
             }
         }
-        int pt=0;
-        while(pt<nums.length){
-            if(red>0){
-                nums[pt]=0;
-                pt++;
-                red--;
-            }
-            else if(white>0){
-                nums[pt]=1;
-                pt++;
-                white--;
-            }
-            else{
-                nums[pt]=2;
-                pt++;
-                blue--;
-            }
-        }
+    }
+    public void swap(int[]arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
