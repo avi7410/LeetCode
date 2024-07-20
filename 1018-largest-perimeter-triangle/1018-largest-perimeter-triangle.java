abcc2c67ae47695137a0598aa58d873a3110523d
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-21 01:01:54 +0530

    Time: 241 ms (5.03%), Space: 45.3 MB (12.73%) - LeetHub

---
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        insertion_sort(nums);
+        for(int i=nums.length-1; i>=2; i--){
+            int a = nums[i];
+            int b = nums[i-1];
+            int c = nums[i-2];
+            if(
+                a+b>c &
+                b+c>a &
+                c+a>b
+            ){
+                return a+b+c;
+            }
+        }
+        return 0;
+    }
+    public void insertion_sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j>0; j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+    public void swap (int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
