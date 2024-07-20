a4f58769e38b56e941804774b0e09c1a3aa3d1e6
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-21 00:43:11 +0530

    Time: 5 ms (15%), Space: 42.8 MB (23.31%) - LeetHub

---
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        insertion_sort(nums);
+        int n = nums.length;
+        return (nums[n-1]-1)*(nums[n-2]-1);
+    }
+    public void insertion_sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j>0; j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+    public void swap (int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
