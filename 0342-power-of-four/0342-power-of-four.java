dedeab26977f5938d2cbd8274f4c612012d3f389
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-21 14:59:37 +0530

    Time: 0 ms (100%), Space: 40.9 MB (18%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return p4c(n,0);
-    }
-    public boolean p4c(long n ,long x){
-        long result = (long)Math.pow(4,x);
-        if(result==n){
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
             return true;
         }
-        else if(result<n){
-            return p4c(n,++x);
-        }
-        return false;
+        return n%4==0 && isPowerOfFour(n/4);
     }
 }
