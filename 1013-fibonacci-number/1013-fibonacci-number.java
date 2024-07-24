56448bb7254181a4b2fd0b2c70a08f4d87397c4a
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-24 18:03:27 +0530

    Time: 0 ms (100%), Space: 40.7 MB (8.97%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,5 @@
 class Solution {
     public int fib(int n) {
-        return Fibonacci(n);
-    }
-    public int Fibonacci (int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return Fibonacci(n-1)+Fibonacci(n-2);
+       return (int) Math.round((Math.pow((1 + Math.sqrt(5)) / 2, n) - Math.pow((1 - Math.sqrt(5)) / 2, n)) / Math.sqrt(5));
     }
 }
