81fdc5b3cc69e6732deb7bde2d757d2bd9b8c9a0
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-25 01:20:35 +0530

    Time: 0 ms (100%), Space: 41.3 MB (87.71%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -1,34 +1,13 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        // Beats 75.91%
-        // int count = 0;
-        // int idx = 0;
-        // StringBuilder ans = new StringBuilder();
-        // while(idx<s.length()){
-        //     if(s.charAt(idx) == ' '){
-        //         count++;
-        //         if(count == k ){
-        //             return ans.toString();
-        //         }
-        //         ans.append(s.charAt(idx));
-        //         idx++;
-        //     }
-        //     else{
-        //         ans.append(s.charAt(idx));
-        //         idx++;
-        //     }
-        // }
-        // return ans.toString();
+        int count = 0, idx = 0;
 
-        String[] arr = s.split(" ");
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0; i < k; i++){
-            ans.append(arr[i]);
-            if(i==k-1){
-                break;
+        while(idx < s.length() && count<k){
+            if(s.charAt(idx++) == ' '){
+                count++;
             }
-            ans.append(" ");
         }
-        return ans.toString();
+
+        return s.substring(0, idx == s.length() ? idx : idx -1);
     }
 }
