7e02345e4f3cbfbd02085b040949eb4d2784d4de
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-25 22:02:48 +0530

    Time: 3 ms (98.68%), Space: 44.7 MB (57.64%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.java
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -1,7 +1,7 @@
 class Solution {
     public String finalString(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.length(); ++i){
             if(s.charAt(i) == 'i'){
                 sb.reverse();
             }
