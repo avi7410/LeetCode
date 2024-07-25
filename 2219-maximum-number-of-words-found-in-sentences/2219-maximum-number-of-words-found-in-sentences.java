6c1c4c3c9c740cb749d24130d651a02413cf524e
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-25 18:42:20 +0530

    Time: 3 ms (86.87%), Space: 44.3 MB (24.08%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -1,19 +1,9 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
         int max = 0;
-
-        for (int i = 0; i < sentences.length; i++){
-            String str = sentences[i];
-            int count = 1;
-
-            for(int j = 0; j < str.length(); j++){
-                if(str.charAt(j) == ' '){
-                    count++;
-                }
-            }
-            max = Math.max(count,max);
+        for(String s : sentences){
+            max = Math.max(max, s.split(" ").length);
         }
-
         return max;
     }
 }
