b624239ea17b1b3af72f4cf03fc8e4782ea9a144
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-26 01:43:09 +0530

    Time: 0 ms (100%), Space: 43.5 MB (16.09%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -3,8 +3,8 @@ class Solution {
         if(nums.length < 2){
             return true;
         }
-        for(int i = 0; i < nums.length-1; i++){
-            if( (nums[i] & 1) == (nums[i+1] & 1)){
+        for(int i = 1; i < nums.length; i++){
+            if( (nums[i]+nums[i-1])%2==0 ){
                 return false;
             }
         }
