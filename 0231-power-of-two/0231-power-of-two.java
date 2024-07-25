6a369fb5363c0a081ccb6253f24bd58bc5fbd88e
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-25 19:23:56 +0530

    Time: 0 ms (100%), Space: 41.2 MB (7.66%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return p2c(n, 0);
-    }
-    public boolean p2c(long n ,long x){
-        long result = (long)Math.pow(2,x);
-        if(result==n){
-            return true;
+        if(n==-2147483648){
+            return false;
         }
-        else if(result<n){
-            return p2c(n,++x);
-        }
-        return false;
+        if(n==0){return false;}
+        return (n & (n-1)) == 0;
     }
 }
