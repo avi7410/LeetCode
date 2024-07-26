2215e5f483baf31f33d0fb4d6995f6bd73ea93cc
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-26 17:54:40 +0530

    Time: 5 ms (10.23%), Space: 45.8 MB (6.66%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        return helper(num1, num2, 0);
+    }
+    private int helper(int a, int b, int count){
+        if(a == 0 || b == 0){
+            return count;
+        }
+        if(a >= b){
+            return helper(a-b, b , ++count);
+        }
+        else{
+            return helper(a, b-a , ++count);
+        }
+    }
+}
