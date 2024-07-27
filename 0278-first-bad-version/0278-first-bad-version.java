dd12300136ee94fefdd6b2332abb86b60f3d2d42
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-27 23:23:02 +0530

    Time: 24 ms (98.2%), Space: 40.3 MB (29.95%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,21 +5,15 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int s=1;
         int e=n;
-        while(s<=n){
+        while(s<e){
             int mid=s+(e-s)/2;
-            if(isBadVersion(mid)!=isBadVersion(mid+1)){
-                return mid+1;
-            }
-            else if(isBadVersion(mid-1)!=isBadVersion(mid)){
-                return mid;
-            }
-            else if(isBadVersion(mid)==true){
-                e=mid-1;
+            if(isBadVersion(mid)){
+                e=mid;
             }
             else{
                 s=mid+1;
             }
         }
-        return 0;
+        return s;
     }
 }
