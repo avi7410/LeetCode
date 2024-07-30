7a499659274805a7c4810df9c6161e49cf5e47f5
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-31 00:10:51 +0530

    Time: 3 ms (5.46%), Space: 45.6 MB (31.7%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -2,18 +2,17 @@ class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         List<Integer> al = new ArrayList<Integer>();
         al.add(0);
-        for(int i = 0; i < flowerbed.length; i++){
-            al.add(flowerbed[i]);
+        for(int i : flowerbed){
+            al.add(i);
         }
         al.add(0);
-        int count = 0;
         for(int i = 1; i < al.size()-1; i++){
             if(al.get(i-1) == 0 && al.get(i) == 0 && al.get(i+1) == 0){
                 i++;
-                count++;
+                n--;
             }
         }
-        if(count >= n) return true;
+        if(n <= 0) return true;
         return false;
     }
 }
