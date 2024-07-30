7c0a575ed8da72374f0ea82ad8fe9a3c6d17ad08
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-30 22:11:48 +0530

    Time: 0 ms (100%), Space: 42.7 MB (34.57%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -2,11 +2,10 @@ class Solution {
     public int countSeniors(String[] details) {
         int count = 0;
         for(String s : details){
-            int age = ((s.charAt(11) - '0')*10) + (s.charAt(12) - '0');
-            if(age>60){
-                count++;
-            }
-
+            int ch1 =s.charAt(11) - '0';
+            int ch2 =s.charAt(12) - '0';
+            int age = ch1 *10 + ch2;
+            if(age>60){count++;}
         }
         return count;
     }
