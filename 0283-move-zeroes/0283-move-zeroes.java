a85ab914c48ce57c2d88cb63cfeff5251add6409
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-07-31 18:46:02 +0530

    Time: 2 ms (83.15%), Space: 46.4 MB (9.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] temp = Arrays.copyOf(nums, nums.length);
+        int[] temp = new int[nums.length];
         int idx = 0;
         for(int i = 0; i < nums.length; i++){
-            if(temp[i] != 0){
-                nums[idx] = temp[i];
+            if(nums[i] != 0){
+                temp[idx] = nums[i];
                 idx++;
             }
         }
-        for(int i = idx; i < nums.length; i++){
-            nums[i] = 0;
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = temp[i];
         }
         return;
 }}
