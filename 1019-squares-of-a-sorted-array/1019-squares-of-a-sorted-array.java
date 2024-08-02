22b859420066c6989d3d18c1b18a07d179e5ce2b
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-03 01:46:20 +0530

    Time: 1 ms (100%), Space: 47 MB (53.89%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,10 +1,42 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        int mid = binarySearchIdx(nums);
         int[] ans = new int[nums.length];
-        for(int i = 0;  i < nums.length; i++){
-            ans[i] = nums[i] * nums[i];
+        int i = mid-1; 
+        int j = mid;
+        int idx = 0;
+        while(i >= 0 && j < nums.length){
+            if(nums[i] * nums[i] < nums[j] * nums[j]){
+                ans[idx++] = nums[i] * nums[i];
+                i--;
+            }
+            else{
+                ans[idx++] = nums[j] * nums[j];
+                j++;
+            }
         }
-        Arrays.sort(ans);
+        while(i >= 0){
+            ans[idx++] = nums[i] * nums[i];
+            i--;
+        }
+        while(j < nums.length){
+            ans[idx++] = nums[j] * nums[j];
+            j++;
+        }
         return ans;
+    }
+    private int binarySearchIdx (int[] nums){
+        int s = 0;
+        int e = nums.length-1;
+        while(s < e){
+            int mid = s + (e - s) / 2;
+            if(nums[mid] < 0 ){
+                s = mid+1;
+            } 
+            else{
+                e = mid;
+            }
+        }
+        return s;
     }
 }
