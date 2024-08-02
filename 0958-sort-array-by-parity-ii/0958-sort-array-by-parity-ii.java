c00744a27bebc20aa00573eb98f55e5f04680e6b
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-03 01:09:29 +0530

    Time: 3 ms (41.27%), Space: 47.6 MB (20.27%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i = 0;
+        int j = 1;
+        while(i < nums.length && j < nums.length){
+            if(nums[i] % 2 == 0){
+                i += 2;
+            }
+            else if(nums[j] % 2 != 0){
+                j += 2;
+            }
+            else{
+                swap(nums, i, j);
+                i += 2;
+                j += 2;
+            }
+        }
+        return nums;
+    }
+    private void swap (int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
