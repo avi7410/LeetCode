45cd27b794175b8126bfb959642404ebfa975794
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-03 17:50:35 +0530

    Time: 2 ms (85.43%), Space: 41.5 MB (19.58%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,6 +1,6 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] original = Arrays.copyOf(heights, heights.length);
+        int[] original = heights.clone();
         Arrays.sort(heights);
         int count = 0;
         for(int i = 0; i < heights.length; i++){
