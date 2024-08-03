08c7e0a5c58a3fba181db82e29ffaa5d8ad2c7de
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-03 17:49:29 +0530

    Time: 2 ms (85.43%), Space: 41.4 MB (19.58%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,36 +1,13 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int count=0;
-        int[] expected = Arrays.copyOf(heights, heights.length);
-        bubble(expected);
-        for(int i=0; i<heights.length; i++){
-            if(expected[i]!=heights[i]){
-                count++;
+        int[] original = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(heights);
+        int count = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(original[i] != heights[i]){
+                count += 1;
             }
         }
         return count;
-    }
-
-    public static void swap (int[] arr, int a, int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
-    }
-
-    public static int[] bubble(int[] arr){
-        int size=arr.length;
-        for(int i=0; i<size-1; i++){
-            boolean swapped = false;
-            for(int j=0; j<size-1-i; j++){
-                if(arr[j]>arr[j+1]){
-                    swapped=true;
-                    swap(arr, j, j+1);
-                }
-            }
-            if(!swapped){
-                break;
-            }
-        }
-        return arr;
     }
 }
