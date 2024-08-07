7a324640bc789a6e14efc0357e1a4b7d166a7aea
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-07 19:40:46 +0530

    Time: 9 ms (42.51%), Space: 43.8 MB (5.01%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String numberToWords(int num) {
+        String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        String[] tens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] others = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        if(num == 0){
+            return "Zero";
+        }
+        else if(num < 10){
+            return ones[num];
+        }
+        else if(num < 20){
+            return tens[num % 10];
+        }
+        else if(num < 100){
+            return others[num / 10] + ((num % 10 == 0) ? ("") : (" " + numberToWords(num % 10)));
+        }
+        else if(num < 1000){
+            return numberToWords(num / 100) + " Hundred" + ((num % 100 == 0) ? ("") : (" " + numberToWords(num % 100)));
+        }
+        else if(num < 1000000){
+            return numberToWords(num / 1000) + " Thousand" + ((num % 1000 == 0) ? ("") : (" " + numberToWords(num % 1000)));
+        }
+        else if(num < 1000000000){
+            return numberToWords(num / 1000000) + " Million" + ((num % 1000000 == 0) ? ("") : (" " + numberToWords(num % 1000000)));
+        }
+        else{
+            return numberToWords(num / 1000000000) + " Billion" + ((num % 1000000000 == 0) ? ("") : (" " + numberToWords(num % 1000000000)));
+        }
+    }
+}
