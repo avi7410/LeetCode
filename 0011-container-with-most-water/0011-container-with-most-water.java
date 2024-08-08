0a868acbef1d5314d62a0a6e253120f0d9ee5bed
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-08 10:29:51 +0530

    Time: 1 ms (100%), Space: 57.7 MB (56.15%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,13 +3,13 @@ class Solution {
         int maxWater = 0;
         int i = 0;
         int j = height.length-1;
-        while(i < j && i < height.length && j >= 0){
+        while(i < j){
             int temp =  Math.min(height[i], height[j]);
             maxWater = Math.max(maxWater, temp * (j - i));
-            while(i < j && height[i] <= temp && i < height.length){
+            while(i < j && height[i] <= temp){
                 i++;
             }
-            while(i < j && height[j] <= temp && j >= 0){
+            while(i < j && height[j] <= temp){
                 j--;
             }
         }
