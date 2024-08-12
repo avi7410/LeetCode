8991bf3c01d4dbe91da6fea1557542eb2ad50e0d
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-12 12:55:07 +0530

    Time: 1 ms (94.43%), Space: 41.3 MB (77.78%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -1,25 +1,27 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        int[][] arr=new int[m][n];
-        
-        for(int i=0;i<indices.length;i++){
-            int j=0;
-                for(int k=0;k<n;k++){
-                    arr[indices[i][j]][k]++;
-                }
-                j++;
-                for(int l=0;l<m;l++){
-                    arr[l][indices[i][j]]++;
-                }
+        int[][] ans = new int[m][n];
+        for(int i = 0; i < indices.length; i++){
+            int x = indices[i][0];
+            int y = indices[i][1];
+            helper(ans, x, y);
         }
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                if(arr[i][j]%2!=0){
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            for(int j : ans[i]){
+                if(j % 2 != 0){
                     count++;
                 }
             }
         }
         return count;
+    }
+    private void helper(int[][] arr, int r, int c){
+        for(int i = 0; i < arr[0].length; i++){
+            arr[r][i] += 1;
+        }
+        for(int i = 0; i < arr.length; i++){
+            arr[i][c] += 1;
+        }
     }
 }
