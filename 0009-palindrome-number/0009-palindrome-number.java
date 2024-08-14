4b0e2c119e3bc23db65fb6c5820b8bd053a1c908
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-14 19:14:38 +0530

    Time: 5 ms (70.89%), Space: 43.8 MB (59.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverseX = 0;
+        int originalX = x;
+        while(x > 0){
+            reverseX = (reverseX * 10) + (x % 10);
+            x /= 10;
+        }
+        return (originalX == reverseX) ? (true) : (false);
+    }
+}
