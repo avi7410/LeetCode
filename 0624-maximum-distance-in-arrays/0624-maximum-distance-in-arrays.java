a417accdc5092f9e334d5df37575fc8848d574d7
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-16 09:52:57 +0530

    Time: 5 ms (100%), Space: 65 MB (18.57%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,14 +1,33 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arrays) {
-        int min = arrays.get(0).get(0);
-        int max = arrays.get(0).get(arrays.get(0).size()-1);
-        int ans = Integer.MIN_VALUE;
-        for(int i = 1; i < arrays.size(); i++){
-            ans = Math.max(ans, Math.abs(min - arrays.get(i).get(arrays.get(i).size()-1)));
-            ans = Math.max(ans, Math.abs(max - arrays.get(i).get(0)));
-            min = Math.min(min, arrays.get(i).get(0));
-            max = Math.max(max, arrays.get(i).get(arrays.get(i).size()-1));
+        int minIdx = -1;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int maxIdx = -1;
+        for(int i = 0; i < arrays.size(); i++){
+            List<Integer> ls = arrays.get(i);
+            int currMax = ls.get(ls.size()-1);
+            if(currMax > max1){
+                max2 = max1;
+                max1 = currMax;
+                maxIdx = i;
+            }
+            else if(currMax > max2){
+                max2 = currMax;
+            }
+
+            int currMin = ls.get(0);
+            if(currMin < min1){
+                min2 = min1;
+                min1 = currMin;
+                minIdx = i;
+            }
+            else if(currMin < min2){
+                min2 = currMin;
+            }
         }
-        return ans;
+        return (minIdx == maxIdx) ? (Math.max(Math.abs(max1 - min2), Math.abs(max2 - min1))) : (Math.abs(max1 - min1));
     }
 }
