0dbf78f55e0d0f1d5ebc2961c6bc40e9c9605d3d
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-08-25 10:27:45 +0530

    Time: 0 ms (100%), Space: 49.4 MB (16.04%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,21 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i=0;
-        int j=s.length-1;
-        rev(s,i,j);
-    }
-    public void rev (char[]s, int i, int j){
-        if(i>=j){
-            return;
+        int i;
+        int j;
+        for(i = 0, j = s.length - 1; i < j; i++, j--){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
-        else{
-            swap(s,i,j);
-            rev(s,i+1,j-1);
-        }
-    }
-    public void swap(char[]s, int i, int j){
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
     }
 }
