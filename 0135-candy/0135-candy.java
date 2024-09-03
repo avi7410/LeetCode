f0bb9aca3557f1d6945d907023ce0422fa932779
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-09-03 13:25:58 +0530

    Time: 2 ms (99.75%), Space: 45.3 MB (71.71%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        for(int i = 0; i < candies.length; i++){
+            candies[i] = 1;
+        }
+        for(int i = 1; i < ratings.length; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.length - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                if(candies[i] <= candies[i + 1]){
+                    candies[i] = candies[i + 1] + 1;
+                }
+            }
+        }
+        int sum = 0;
+        for(int candie : candies){
+            sum += candie;
+        }
+        return sum;
+    }
+}
