19246d06e19eda045fea411581230bb5adbbfd7a
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-09-03 09:57:44 +0530

    Time: 3 ms (37.62%), Space: 41.3 MB (93.49%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        int  num = 0;
+        for(int i = 0; i < s.length(); i++){
+            int temp = (int)s.charAt(i) - 'a' + 1;
+            num += transform(temp, 1);
+        }
+        k--;
+        System.out.println(num);
+        return transform(num, k);
+        
+    }
+    private int transform(int num, int k){
+        if(k == 0){
+            return num;
+        }
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        return transform(sum, k - 1);
+    }
+}
