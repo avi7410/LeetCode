8eb42118565d674a4177b50294f5f393dc8adf11
Avi Vyas <115559224+avi7410@users.noreply.github.com>
2024-09-23 21:25:25 +0530

    Time: 1 ms (100%), Space: 44.2 MB (72.23%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,7 +1,17 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int leftover = money - prices[0] - prices[1];
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for(int price : prices){
+            if(price < min1){
+                min2 = min1;
+                min1 = price;
+            }
+            else if(price < min2){
+                min2 = price;
+            }
+        }
+        int leftover = money - (min1 + min2);
         return (leftover < 0) ? (money) : (leftover);
     }
 }
